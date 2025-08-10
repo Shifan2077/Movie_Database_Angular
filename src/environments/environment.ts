77c5d3319b09ecbe3b2b34994bdb560d6b0a7830
Shifan2077 <nagarjishifan@gmail.com>
2025-08-10 22:13:22 +0530

    key-lock-3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    supabaseUrl: process.env['NG_APP_SUPABASE_URL'],
-    supabaseAnonKey: process.env['NG_APP_SUPABASE_ANON_KEY'],
+    supabaseUrl: process.env['NG_APP_SUPABASE_URL'] ?? '',
+    supabaseAnonKey: process.env['NG_APP_SUPABASE_ANON_KEY'] ?? '',
     tmdbApiKey: '9975187a3fab275529f9b6654a210084',
     watchmodeApiKey: 'mdrpzWZyYCDICZ6T9NAp1mqUqCCKX0XKEcHmcPDk',
   };
